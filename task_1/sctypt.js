c0d8c5f73c3007904b44db1f45c7652e468225f1
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-07-25 10:06:41 +0300

    Done with Task 1

---
+++ task_1/sctypt.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // This code will run after the HTML document has been fully loaded
+    const heading = document.querySelector("h1");
+    heading.textContent = "Hello from External JavaScript!";
+});
