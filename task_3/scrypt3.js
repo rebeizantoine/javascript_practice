36ee1894b97ccc24d9a3ac6ba6cf986cd12068fa
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-07-25 16:07:45 +0300

    Done with task 3

---
+++ task_3/scrypt3.js
@@ -0,0 +1,17 @@
+const num1 = 4;
+const num2 = 1;
+const canPass = true
+if (canPass) {
+    console.log("enter to check the next condition");
+} else {
+    console.log("Try again");
+
+}
+
+if (num1 >= num2) {
+    console.log("Hello");
+} else {
+    console.log("Bye");
+
+}
+
