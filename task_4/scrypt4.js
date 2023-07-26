8a0ec7b2e00e9b90f00ac00e04cfb87e0868424b
Antoine Rebeiz <rebeizantoine@gmail.com>
2023-07-26 10:21:32 +0300

    Done with Task 4

---
+++ task_4/scrypt4.js
@@ -0,0 +1,6 @@
+const NumberArray = [1, 2, 3];
+console.log("NumberArray:", NumberArray)
+const StringArray = ["antoine", "abdo", "rebeiz"];
+console.log("StringArray:", StringArray)
+const MixedArray = [1, "hello", 2, "Bye"]
+console.log("MixedArray:", MixedArray)
